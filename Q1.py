de6828b462dbf57bc26d390db1ed078ae48d4893
AYELET <ayeletorot4@gmail.com>
2026-03-29 18:01:43 +0300

    done

---
+++ Q1.py
@@ -0,0 +1,30 @@
+
+def get_grade():
+    """
+
+    :return:
+    """
+    valid_grade = []
+    while True:
+        try:
+            grade = int(input('Enter grade: '))
+            if grade == -999:
+                if len(valid_grade) < 10:
+                    print('Need at least 10 valid grades, keep entering')
+                    continue
+                break
+            if grade < 0 or grade > 100:
+                raise Exception('Not in range')
+            valid_grade.append(grade)
+
+
+        except ValueError:
+            print('Invalid input,skip')
+        except Exception:
+            print('Not in range,skip')
+
+    print(f"Number of valid grades {len(valid_grade)}")
+    print(f"Class average {sum(valid_grade)/len(valid_grade)}")
+    print(f"Highest grade {max(valid_grade)}")
+
+get_grade()
