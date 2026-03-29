7cee6acb828fc0a8bdfdb1c3b3a5c3b5af036348
AYELET <ayeletorot4@gmail.com>
2026-03-29 21:30:06 +0300

    done

--- Q5.py
+++ Q5.py
@@ -1,3 +1,4 @@
+#unfinished
 def most_common_word(story: tuple[str,...])->str:
     """
     get a story and find which word appeared the most times in that story
@@ -10,10 +11,26 @@ def most_common_word(story: tuple[str,...])->str:
         lst1= i.split()
         list_.extend(lst1)
 
-    print(list_)
-    my_story=tuple(list_)
-    for word in my_story:
-        n=my_story.count(word)
-        print(word,n)
+
+    tuple_ = tuple(list_)
+    set_=set(list_)
+    dict_={}
+    for i in set_:
+        n = tuple_.count(i)
+        dict_[i]=n
+
+
+    maximum = max(dict_.values())
+    for k,v in dict_.items():
+        if v==maximum:
+            print(k)
+            print(f"{maximum} times")
+
+
 
 most_common_word(("hello world", "hello there- today is sunday","weekend is fun","yes it is"))
+most_common_word(story = (
+    "The little fox saw the little bird and the little cat.",
+    "The fox was happy because the little bird sang, and the little cat jumped.",
+    "The little fox, the little bird, and the little cat became friends."
+))
